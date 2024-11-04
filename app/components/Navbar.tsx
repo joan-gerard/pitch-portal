1e8d959f4c0c085055d7f9faa90f6385b4b0b560
Joan Gerard <joan.gerard@outlook.com>
2024-11-04 15:52:06 +0100

    set up Next Auth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <header className="px-5 pr-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={() => signOut()}>
+                <span>Logout</span>
+              </button>
+              {/* <Link href={`/user/${session?.id}`}>
+                <span>{session.user.name}</span>
+              </Link> */}
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
