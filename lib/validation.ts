344d434a71c2740aa9e5e5954199c0b156144180
Joan Gerard <joan.gerard@outlook.com>
2024-11-08 18:51:59 +0100

    fix deplyment errors

--- lib/validation.ts
+++ lib/validation.ts
@@ -13,6 +13,7 @@ export const formSchema = z.object({
         const contentType = res.headers.get("content-type");
         return contentType?.startsWith("image/");
       } catch (error) {
+        console.log(error);
         return false;
       }
     }),
